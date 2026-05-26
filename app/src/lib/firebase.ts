3eb05befa1475d7dfb8743514900735823bcceb7
Mutee Ur Rehman <muteestacks@gmail.com>
2026-05-26 20:42:55 +0500

    Update firebase.ts

--- app/src/lib/firebase.ts
+++ app/src/lib/firebase.ts
@@ -3,14 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-63cCbWGde6pG3Y9WOpLaV7WlriBFMkU",
-  authDomain: "task-manager-b32ac.firebaseapp.com",
-  databaseURL: "https://task-manager-b32ac-default-rtdb.firebaseio.com",
-  projectId: "task-manager-b32ac",
-  storageBucket: "task-manager-b32ac.firebasestorage.app",
-  messagingSenderId: "45224753095",
-  appId: "1:45224753095:web:a81342387d1f5d35a24be2",
-  measurementId: "G-8YQ2LSNXS1"
+
 };
 
 export const app = initializeApp(firebaseConfig);
